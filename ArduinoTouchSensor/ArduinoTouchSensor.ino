e856a4ee0f91f1f6a2048d2e235726ca9532f812
Caprixis <fluffylemons213@gmail.com>
2025-11-06 20:24:17 -0500

    Changed Comments

--- ArduinoTouchSensor/ArduinoTouchSensor.ino
+++ ArduinoTouchSensor/ArduinoTouchSensor.ino
@@ -1,5 +1,5 @@
 #include <Adafruit_CircuitPlayground.h>
-//Code for a touch sensor. Depending on the Connection Pad that is touched, a color will flash on a random Neopixel, moving to different neopixel's depending on movement in the environment around it.
+//Code for a touch sensor. Depending on the Connection Pad that is touched, a color willl flash randomly across all the Neopixels.
 int value;
 int value2;
 int value3;
@@ -16,14 +16,13 @@ void loop() {
   CircuitPlayground.clearPixels();
   // put your main code here, to run repeatedly:
   value = analogRead(A3);
-  Serial.println(value);
   value2 = analogRead(A2);
   value3 = analogRead(A1);
   
   //these lines make it so that the corresponding color is randomly flashed on the neopixels
-  a=map(value, 0, 1023, 0, 9);
-  b=map(value2, 0, 1023, 0, 9);
-  c=map(value3, 0, 1023, 0, 9);
+  a=map(value, 0, 1023, 100, 1000);
+  b=map(value2, 0, 1023, 100, 1000);
+  c=map(value3, 0, 1023, 100, 1000);
 
   //The lines below indicate what color flashes on the neopixels (in order it is red, green, and blue)
   CircuitPlayground.setPixelColor(a, 255, 0, 0);
